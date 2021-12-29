05e5ff814a6b31294d05f64afe128e290b22a1d2
Zoya Khan <95264720+zoyakhan184@users.noreply.github.com>
2021-12-29 13:44:04 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import Users from "./components/cards";
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { users_data: [], loading: false };
+    this.updateState = this.updateState.bind(this);
+  }
+
+  updateState() {
+    this.setState({ loading: true });
+
+    setTimeout(
+      async function () {
+        const response = await fetch("https://reqres.in/api/users?page=1");
+        const jsonresponse = await response.json();
+        console.log(jsonresponse);
+        this.setState({ users_data: jsonresponse["data"], loading: false });
+      }.bind(this),
+      2500
+    );
+  }
+  
+  render() {
+    return (
+      <>
+        <nav className="navbar">
+          <div className="navitems">
+            <h2><b>User Profile App</b></h2>
+            
+            <button className="fetchbtn" onClick={this.updateState}>
+              <b>Get Users</b>
+            </button>
+          </div>
+        </nav>
+        <br/><br/>
+        <div className="userdatacontainer">
+          <Users loading={this.state.loading} users={this.state.users_data} />
+        </div>
+        <div class="container my-50">
+          
+        <br/><br/>
+        <footer className="footer">
+          Created By &copy; Zoya Khan 
+        </footer>
+        <footer className="footer">
+          LetsGrowMore - WebDev Intern
+        </footer>
+        <br/><br/>
+        </div>
+      </>
+    );
+  }
 }
-
 export default App;
